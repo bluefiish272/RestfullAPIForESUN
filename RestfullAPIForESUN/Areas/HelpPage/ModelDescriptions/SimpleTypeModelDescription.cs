da34409507302ffa95ea97b32299c9ec7fd7f562
user <user@user-PC>
2017-02-14 22:06:42 +0800

    加入專案檔案。

---
+++ RestfullAPIForESUN/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestfullAPIForESUN.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RestfullAPIForESUN/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RestfullAPIForESUN.Startup))]
+
+namespace RestfullAPIForESUN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
